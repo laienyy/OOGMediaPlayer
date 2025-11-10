e27b168af9bc7d5763288ff51c9dde8fb2a66b31
yy <yiyuan@laien.io>
2025-11-10 20:29:06 +0800

    公开init方法

--- OOGMediaPlayer/Sources/OOGAudioPlayer/DownloadRequest.swift
+++ OOGMediaPlayer/Sources/OOGAudioPlayer/DownloadRequest.swift
@@ -30,7 +30,7 @@ public class DownloadRequest: NSObject {
     var isCanceled: Bool = false
     var progressHandler: ProgressHandler?
     
-    init(url: URL, timeoutInterval: TimeInterval = 60, debugInfo: String) {
+    public init(url: URL, timeoutInterval: TimeInterval = 60, debugInfo: String) {
         self.url = url
         self.timeoutInterval = timeoutInterval
         self.debugInfo = debugInfo
@@ -40,7 +40,7 @@ public class DownloadRequest: NSObject {
         log(prefix: .mediaPlayer, "Download Request released -", debugInfo)
     }
     
-    func fetchDataInProgress(progress: ProgressHandler?) async throws -> Data {
+    public func fetchDataInProgress(progress: ProgressHandler?) async throws -> Data {
         self.progressHandler = progress
         
         // 增加超时
@@ -122,69 +122,3 @@ public class DownloadRequest: NSObject {
         isCanceled = true
     }
 }
-
-//extension URLSession {
-//    
-//    func download(url: URL) async throws -> Data {
-//        
-//        let (fileTempUrl, response) = try await URLSession.shared.download(from: url)
-//        
-//        guard let httpResponse = response as? HTTPURLResponse /* OK */ else {
-//            throw NSError(domain: "HTTP request response invalid", code: -1)
-//        }
-//        
-//        guard httpResponse.statusCode == 200 else {
-//            throw NSError(domain: "HTTP request response code invalid", code: httpResponse.statusCode)
-//        }
-//        
-//        return try Data(contentsOf: fileTempUrl)
-//    }
-//    
-//    func fetchDataInProgress(url: URL, progress: ProgressHandler? = nil) async throws -> Data {
-//        let progressHandler = progress
-//        
-//        log(prefix: .mediaPlayer, "[Download] Start downloading - \(url.relativePath)")
-//        let (asyncBytes, urlResponse) = try await URLSession.shared.bytes(from: url)
-//        
-//        let length = (urlResponse.expectedContentLength)
-//        var data = Data()
-//        data.reserveCapacity(Int(length))
-//        
-//        let progress = Progress(totalUnitCount: length)
-//        
-//        // 有必要回调的进度最小变化
-//        let callbackGranularity = Int(Double(length) * 0.002)
-//        
-//        var preProgress: Int = 0
-//        
-//        
-//        for try await byte in asyncBytes {
-//            
-//            data.append(byte)
-//            
-//            if let handler = progressHandler {
-//                
-//                let diff = data.count - preProgress
-//                // 降低回调频率，过滤回调次数过多的问题
-//                let isNesessaryToCallback = diff > callbackGranularity || data.count == length
-//                guard isNesessaryToCallback else {
-//                    continue
-//                }
-//                
-//                preProgress = Int(data.count)
-//                progress.completedUnitCount = Int64(data.count)
-//                
-//                log(prefix: .mediaPlayer, String(format: "[Download] Progress: %ld / %ld - %.1f%%",
-//                                         progress.completedUnitCount,
-//                                         progress.totalUnitCount,
-//                                         progress.percentComplete * 100))
-////                
-//                handler.queue.async { progressHandler?.callback(progress) }
-//            }
-//        }
-//        
-//        log(prefix: .mediaPlayer, "Downloading finished - \(url.relativePath) (\(data.count / 1024) KB)")
-//        
-//        return data
-//    }
-//}
